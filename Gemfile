efd822799b20605e1efd80ec06e482efe84df291
Betzabe Arenas <bekkhaaa@gmail.com>
2017-05-30 19:21:07 -0500

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -1,13 +1,17 @@
 source 'https://rubygems.org'
 
+
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
 
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
+# Gem for used bootstrap
+gem 'bootstrap-sass', '3.3.6'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 # Use Puma as the app server
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,39 +6,39 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
-  test "should get root" do
-    get static_pages_home_url
-    assert_response :success
-  end
+  # test "should get root" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  # end
 
+# test "should get help" do
+#     # get help_path
+#     get static_pages_help_url
+#     assert_response :success
+#     assert_select "title", "Help | #{@base_title}"
+#   end
   # this is a test
-  test "should get home" do
-    get static_pages_home_url
+ test "should get home" do
+    get root_path
     assert_response :success
-    # checks for the presence of a <title> tag containing the string “Home | Ruby on Rails Tutorial Sample App”
-    # code without refactoring
-    # assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-    # # Refactoring line 13
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-
     assert_select "title", "About | #{@base_title}"
   end
-  # test for the existence of a page at the URL /static_pages/contact by testing for the title “Contact | Ruby on Rails Tutorial Sample App”
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
 
+  test "should get contact" do
+    get contact_path
+    assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
 
